1f5fb8cb881ecca683331b801da5d9c133ed0867
Matin <zivdar1matin@gmail.com>
2020-11-07 18:58:04 +0330

    Fix bug founded with alu_tb1 in funct 0001 and 0010) by changing IF/ELSE -> WITH/SELECT/WHEN

--- GenericALU/alu.vhd
+++ GenericALU/alu.vhd
@@ -32,68 +32,56 @@ ARCHITECTURE arch OF alu IS
     -- Design decision
     CONSTANT c_zero : signed(g_size -1 DOWNTO 0) := (others => '0');
 
-    SIGNAL s_AplusB         : signed(g_size DOWNTO 0);
-    SIGNAL s_AplusBplusCin  : signed(g_size DOWNTO 0);
+    SIGNAL s_AplusB             : signed(g_size DOWNTO 0);
+    SIGNAL s_AplusB_Ov          : std_logic;
+    SIGNAL s_AplusBplusCin      : signed(g_size DOWNTO 0);
+    SIGNAL s_AplusBplusCin_Ov   : std_logic;
+    SIGNAL s_GT                 : std_logic;
+    SIGNAL s_LT                 : std_logic;
+    SIGNAL s_EQ                 : std_logic;
 
 BEGIN
-    s_AplusB <= ('0' & p_A) + ('0' & p_B);
-    s_AplusBplusCin <= ('0' & p_A) + ('0' & p_B) + p_cin;
-    PROCESS(p_A, p_B, p_cin, p_sin, p_funct)
-    BEGIN
-        IF p_funct = "0000" THEN
-            p_Z <= NOT p_B(g_size - 1) & p_B(g_size - 2 DOWNTO 0);
-        ELSIF p_funct = "0001" THEN
-            p_Z <= s_AplusB(g_size - 1 DOWNTO 0);
-            p_cout <= s_AplusB(g_size);
-            p_Ov <= (p_A(g_size - 1) AND p_B(g_size - 1) AND NOT s_AplusB(g_size - 1)) OR
-                (NOT p_A(g_size - 1) AND NOT p_B(g_size - 1) AND s_AplusB(g_size - 1));
-        ELSIF p_funct = "0010" THEN
-            p_Z <= s_AplusBplusCin(g_size - 1 DOWNTO 0);
-            p_cout <= s_AplusBplusCin(g_size);
-            p_Ov <= (p_A(g_size - 1) AND p_B(g_size - 1) AND NOT s_AplusBplusCin(g_size - 1)) OR
-                (NOT p_A(g_size - 1) AND NOT p_B(g_size - 1) AND s_AplusBplusCin(g_size - 1));
-        ELSIF p_funct = "0011" THEN
-            p_Z <= p_A - p_B;
-        ELSIF p_funct = "0100" THEN
-            p_Z <= -p_B;
-        ELSIF p_funct = "0101" THEN
-            p_Z <= NOT p_B;
-        ELSIF p_funct = "0110" THEN
-            p_Z <= p_A AND p_B;
-        ELSIF p_funct = "0111" THEN
-            p_Z <= p_A OR p_B;
-        ELSIF p_funct = "1000" THEN
-            p_Z <= p_A XOR p_B;
-        ELSIF p_funct = "1001" THEN
-            p_Z <= p_A(g_size  - 2 DOWNTO 0) & p_sin;
-            p_sout <= p_A(g_size  - 1);
-        ELSIF p_funct = "1010" THEN
-            p_Z <= p_sin & p_A(g_size  - 1 DOWNTO 1);
-            p_sout <= p_A(0);
-        ELSIF p_funct = "1011" THEN
-            p_Z <= p_A rol 1;
-        ELSIF p_funct = "1100" THEN
-            p_Z <= p_A ror 1;
-        ELSIF p_funct = "1101" THEN
-            IF p_A > p_B THEN
-                p_Z <= c_zero + 1;
-            ELSE
-                p_Z <=  c_zero;
-            END IF;
-        ELSIF p_funct = "1110" THEN
-            IF p_A < p_B THEN
-                p_Z <= c_zero + 1;
-            ELSE
-                p_Z <= c_zero;
-            END IF;
-        ELSIF p_funct = "1111" THEN
-            IF p_A = p_B THEN
-                p_Z <= c_zero + 1;
-            ELSE
-                p_Z <= c_zero;
-            END IF;
-        ELSE
-            p_Z <= c_zero;
-        END IF;
-    END PROCESS;
+    s_AplusB    <=  ('0' & p_A) + ('0' & p_B);
+    s_AplusB_Ov <=  (p_A(g_size - 1) AND p_B(g_size - 1) AND NOT s_AplusB(g_size - 1)) OR
+                    (NOT p_A(g_size - 1) AND NOT p_B(g_size - 1) AND s_AplusB(g_size - 1));
+
+    s_AplusBplusCin     <=  ('0' & p_A) + ('0' & p_B) + p_cin;
+    s_AplusBplusCin_Ov  <=  (p_A(g_size - 1) AND p_B(g_size - 1) AND NOT s_AplusBplusCin(g_size - 1)) OR
+                            (NOT p_A(g_size - 1) AND NOT p_B(g_size - 1) AND s_AplusBplusCin(g_size - 1));
+
+    s_GT <= '1' WHEN p_A>p_B ELSE '0';
+    s_LT <= '1' WHEN p_A<p_B ELSE '0';
+    s_EQ <= '1' WHEN p_A=p_B ELSE '0';
+
+    WITH p_funct SELECT
+        p_Z <=  NOT p_B(g_size - 1) & p_B(g_size - 2 DOWNTO 0)  WHEN "0000",
+                s_AplusB(g_size - 1 DOWNTO 0)                   WHEN "0001",
+                s_AplusBplusCin(g_size - 1 DOWNTO 0)            WHEN "0010",
+                p_A - p_B                                       WHEN "0011",
+                -p_B                                            WHEN "0100",
+                NOT p_B                                         WHEN "0101",
+                p_A ror 1                                       WHEN "0110",
+                p_A OR p_B                                      WHEN "0111",
+                p_A XOR p_B                                     WHEN "1000",
+                p_A(g_size  - 2 DOWNTO 0) & p_sin               WHEN "1001",
+                p_sin & p_A(g_size  - 1 DOWNTO 1)               WHEN "1010",
+                p_A rol 1                                       WHEN "1011",
+                p_A ror 1                                       WHEN "1100",
+                c_zero(g_size-2 DOWNTO 0) & s_GT                WHEN "1101",
+                c_zero(g_size-2 DOWNTO 0) & s_LT                WHEN "1110",
+                c_zero(g_size-2 DOWNTO 0) & s_EQ                WHEN "1111",
+                c_zero                                          WHEN OTHERS;
+    
+    WITH p_funct SELECT
+        p_cout  <=  s_AplusB(g_size)        WHEN "0001",
+                    s_AplusBplusCin(g_size) WHEN "0010",
+                    '0'                     WHEN OTHERS;
+    WITH p_funct SELECT
+        p_Ov    <=  s_AplusB_OV         WHEN "0001",
+                    s_AplusBplusCin_Ov  WHEN "0010",
+                    '0'                 WHEN OTHERS;
+    WITH p_funct SELECT
+        p_sout  <=  p_A(g_size  - 1)    WHEN "1001",
+                    p_A(0)              WHEN "1010",
+                    '0'                 WHEN OTHERS;
 END arch;
