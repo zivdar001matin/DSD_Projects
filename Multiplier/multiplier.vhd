23f003e612dcc6e308d1f5a731ec1e8a2382f57c
Matin <zivdar1matin@gmail.com>
2020-11-15 05:33:32 +0330

    Complete multiplier

--- Multiplier/multiplier.vhd
+++ Multiplier/multiplier.vhd
@@ -30,10 +30,10 @@ ARCHITECTURE combinational OF mult IS
         );
     END COMPONENT;
 -- Design desicions
-    TYPE Type_and IS ARRAY (g_size-1 DOWNTO 0) OF std_logic_vector(g_size-1 DOWNTO 0); 
+    TYPE Type_and IS ARRAY (2*g_size-2 DOWNTO 0) OF std_logic_vector(g_size-1 DOWNTO 0); 
     SIGNAL s_and  : Type_and;
     -- s_carry(i)(j) represents i column, j row
-    TYPE Type_carry IS ARRAY (g_size DOWNTO 0) OF std_logic_vector(g_size DOWNTO 0); 
+    TYPE Type_carry IS ARRAY (2*g_size-3 DOWNTO 0) OF std_logic_vector(g_size DOWNTO 0); 
     SIGNAL s_carry : Type_carry;
     -- s_sum(i)(j) represents i row, j column 
     TYPE Type_sum IS ARRAY (g_size+1 DOWNTO 0) OF std_logic_vector(g_size DOWNTO 0); 
@@ -45,14 +45,19 @@ BEGIN
             s_and(i)(j) <= p_a(i) AND p_b(j);
         END GENERATE Init_L2;
     END GENERATE Init_L1;
-    s_sum(0)(g_size-1 DOWNTO 0) <= s_and(0);
-    s_sum(0)(g_size) <= '0';
-    L1: FOR i IN 1 TO g_size-1 GENERATE
+    Init2_L1: FOR i IN g_size TO 2*g_size-2 GENERATE
+        s_and(i) <= (others=>'0');
+    END GENERATE Init2_L1;
+    s_sum(0) <= '0' & s_and(0);
+    p_s(0) <= s_sum(0)(0);
+    L1: FOR i IN 1 TO 2*g_size-3 GENERATE
         L2: FOR j IN 0 TO g_size-1 GENERATE
-            u1 : fa PORT MAP(s_and(j)(i), s_sum(i-1)(j+1), s_carry(j)(i-1),
+            u1 : adder PORT MAP(s_and(i)(j), s_sum(i-1)(j+1), s_carry(j)(i-1),
                             s_sum(i)(j), s_carry(j+1)(i-1));
         END GENERATE L2;
         s_sum(i)(g_size) <= s_carry(g_size)(i-1);
         p_s(i) <= s_sum(i)(0);
     END GENERATE L1;
+    p_s(2*g_size-1 DOWNTO 2*g_size-2) <= s_sum(2*g_size-3)(2 DOWNTO 1);
+    p_cout <= s_carry(g_size)(g_size);
 END combinational;
